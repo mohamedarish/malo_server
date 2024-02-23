bf69f3186c325eb05aab9fef9c679221f38ee934
mohamed arish <arish1248@gmail.com>
2024-02-23 14:55:04 +0530

    Should find words that have multiple listings now

--- src/data/getMeaning.ts
+++ src/data/getMeaning.ts
@@ -4,7 +4,9 @@ export const getMeaning = async (words: string[]) => {
     const meanings: string[] = [];
 
     for (let i = 0; i < words.length; i += 1) {
-        const meaning = await datuk.findOne({ entry: `^${words[i]}[0-9]+` });
+        const meaning = await datuk.findOne({
+            entry: { $regex: `^${words[i]}[0-9]+` },
+        });
 
         if (meaning) {
             meaning.defs.forEach((mean) => {
