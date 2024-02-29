6c410eaa09d7f9fcb3d82785bb0da434797efd29
mohamed arish <arish1248@gmail.com>
2024-02-29 20:35:21 +0530

    Regex errors are handled now'

--- src/data/getMeaning.ts
+++ src/data/getMeaning.ts
@@ -5,26 +5,30 @@ export const getMeaning = async (words: string[]) => {
     for (let i = 0; i < words.length; i += 1) {
         const m: string[][] = [];
 
-        const meaning = await datuk.find({
-            entry: { $regex: `^${words[i]}[0-9]*$` },
-        });
-
-        if (meaning) {
-            meaning.forEach((mean) => {
-                const ms: string[] = [];
-
-                mean.defs.forEach((mean) => {
-                    ms.push(mean.entry);
-                });
-
-                m.push(ms);
+        try {
+            const meaning = await datuk.find({
+                entry: { $regex: `^${words[i]}[0-9]*$` },
             });
-        }
 
-        meanings.push({
-            word: words[i],
-            meanings: m,
-        });
+            if (meaning) {
+                meaning.forEach((mean) => {
+                    const ms: string[] = [];
+
+                    mean.defs.forEach((mean) => {
+                        ms.push(mean.entry);
+                    });
+
+                    m.push(ms);
+                });
+            }
+
+            meanings.push({
+                word: words[i],
+                meanings: m,
+            });
+        } catch (err) {
+            continue;
+        }
     }
 
     return meanings;
